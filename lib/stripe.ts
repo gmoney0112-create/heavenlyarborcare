3813f41da0d92c542e174279a8fec4dbb7b164ba
Terrance Gee <gmoney0112@gmail.com>
2026-04-23 02:21:16 -0500

    fix: lazy-init Stripe client to prevent module-level env error at build

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,5 +1,20 @@
 import Stripe from 'stripe'
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-09-30.acacia',
-})
+let _stripe: Stripe | null = null
+
+export function getStripe(): Stripe {
+  if (!_stripe) {
+    _stripe = new Stripe(process.env.STRIPE_SECRET_KEY || 'sk_test_placeholder', {
+      apiVersion: '2024-09-30.acacia',
+    })
+  }
+  return _stripe
+}
+
+// Keep backward compat export
+export const stripe = { ...{} } as unknown as Stripe
+Object.defineProperty(stripe, 'checkout', { get: () => getStripe().checkout })
+Object.defineProperty(stripe, 'paymentIntents', { get: () => getStripe().paymentIntents })
+Object.defineProperty(stripe, 'webhooks', { get: () => getStripe().webhooks })
+Object.defineProperty(stripe, 'prices', { get: () => getStripe().prices })
+Object.defineProperty(stripe, 'products', { get: () => getStripe().products })
